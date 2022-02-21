a6eb6e824c07d6b377bdc85968435dda2888f23c
GustavoGEL <92034319+GustavoGEL@users.noreply.github.com>
2022-02-21 21:29:40 +0000

    Add files via upload

---
+++ W_mass_runs/3_bare_and_photos_weight/scripts/run.sh
@@ -0,0 +1,5 @@
+# Load root
+source /cvmfs/lhcb.cern.ch/lib/etc/cern_profile.sh
+source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_100 x86_64-centos7-gcc10-opt
+# Run python script
+python3 ./"W_mass_calculator (bare_and_photos).py"
