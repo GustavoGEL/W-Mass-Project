71a4114df96389b3ff0b52a5bcf6f682636aa6e2
GustavoGEL <92034319+GustavoGEL@users.noreply.github.com>
2022-06-02 15:51:34 +0100

    Delete W_mass_runs directory

--- W_mass_runs/5.3_bare_muon_pt_highest_pt_photos_w/scripts/run.sh
+++
@@ -1,5 +0,0 @@
-# Load root
-source /cvmfs/lhcb.cern.ch/lib/etc/cern_profile.sh
-source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_100 x86_64-centos7-gcc10-opt
-# Run python script
-python3 ./"W_mass_calculator (bare_highest_photon_and_photos).py"
