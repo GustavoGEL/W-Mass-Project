bc9e3ab495a0ed14d8a9172d2a1a8b1aaf044e0c
GustavoGEL <92034319+GustavoGEL@users.noreply.github.com>
2022-06-02 15:51:24 +0100

    Delete W_and_photons_calculator directory

--- W_and_photons_calculator/prev_tries/2_photons/scripts/run.sh
+++
@@ -1,5 +0,0 @@
-# Load root
-source /cvmfs/lhcb.cern.ch/lib/etc/cern_profile.sh
-source /cvmfs/sft.cern.ch/lcg/views/setupViews.sh LCG_100 x86_64-centos7-gcc10-opt
-# Run python script
-python3 ./"W_mass_calculator (2_photon_pt).py"
