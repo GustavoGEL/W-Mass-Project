5fbb02cfe683b4bb811c2e4356a42098475f063d
GustavoGEL <92034319+GustavoGEL@users.noreply.github.com>
2022-06-02 15:50:53 +0100

    Delete PastTrials directory

--- PastTrials/ScriptTesting/LinxTextFiles/c_test.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int test_num = 195;
-    printf("Hello, working and number is %d\n", test_num);
-}
--- PastTrials/VenvTest.py
+++
@@ -1,4 +0,0 @@
-import time
-
-print("Hello, installed venv works!")
-time.sleep(3)  # Wait 3s to visualise, in case that the program runs in a new window
